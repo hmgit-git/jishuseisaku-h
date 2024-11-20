5674af63f346300f3ba6274849a033b197a7de6f
hmgit-git <hamagamimiyoko@gmail.com>
2024-11-20 21:30:55 +0900

    1120ZZ

--- routes/web.php
+++ routes/web.php
@@ -21,39 +21,23 @@ use Illuminate\Support\Facades\Route;
 Auth::routes();
 
 Route::get('/', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+Route::get('/users', [App\Http\Controllers\UserController::class, 'index'])->name('users.index');
+Route::get('/items', [App\Http\Controllers\ItemController::class, 'index'])->name('items.index');
 
-Route::get('/', [App\Http\Controllers\HomeController::class, 'index']);
 Route::get('/news', [App\Http\Controllers\NewsController::class, 'index'])->name('news.index'); // ニュース一覧
 Route::post('/news', [App\Http\Controllers\NewsController::class, 'store'])->name('news.store'); // ニュース追加
 Route::delete('/news/{id}', [App\Http\Controllers\NewsController::class, 'destroy'])->name('news.destroy'); // ニュース削除
 
-
 Route::prefix('items')->group(function () {
-    Route::get('/', [App\Http\Controllers\ItemController::class, 'index'])->name('items.index');
     Route::get('/add', [App\Http\Controllers\ItemController::class, 'add']);
     Route::post('/add', [App\Http\Controllers\ItemController::class, 'add']);
     Route::put('/update/{id}', [App\Http\Controllers\ItemController::class, 'update'])->name('items.update');
     Route::delete('/items/{id}/destroy', [App\Http\Controllers\ItemController::class, 'destroy'])->name('items.destroy');
     Route::get('/items/{id}/edit', [App\Http\Controllers\ItemController::class, 'edit'])->name('items.edit');
-
-    Route::get('/search', [App\Http\Controllers\ItemController::class, 'search']);
-
-  
-
 });
-
-
-
-
-
-    // 管理者の編集画面
-
 // 編集画面表示
 Route::get('/users/{id}/edit', [App\Http\Controllers\UserController::class, 'edit'])->name('users.edit');
-
 // 更新処理
 Route::put('/users/{id}', [App\Http\Controllers\UserController::class, 'update'])->name('users.update');
-
-// 一覧表示と削除ルート（必要に応じて）
-Route::get('/users', [App\Http\Controllers\UserController::class, 'index'])->name('users.index');
+// 削除ルート（必要に応じて）
 Route::delete('/users/{id}/destroy', [App\Http\Controllers\UserController::class, 'destroy'])->name('users.destroy');
