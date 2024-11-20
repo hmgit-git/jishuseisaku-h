43f0359b855eea8539a3f9c20c8c99f7f36e6a79
hmgit-git <hamagamimiyoko@gmail.com>
2024-11-20 21:41:29 +0900

    1120xx

--- routes/web.php
+++ routes/web.php
@@ -19,25 +19,29 @@ use Illuminate\Support\Facades\Route;
 // });
 
 Auth::routes();
-
-Route::get('/', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
-Route::get('/users', [App\Http\Controllers\UserController::class, 'index'])->name('users.index');
-Route::get('/items', [App\Http\Controllers\ItemController::class, 'index'])->name('items.index');
-
-Route::get('/news', [App\Http\Controllers\NewsController::class, 'index'])->name('news.index'); // ニュース一覧
-Route::post('/news', [App\Http\Controllers\NewsController::class, 'store'])->name('news.store'); // ニュース追加
-Route::delete('/news/{id}', [App\Http\Controllers\NewsController::class, 'destroy'])->name('news.destroy'); // ニュース削除
-
-Route::prefix('items')->group(function () {
-    Route::get('/add', [App\Http\Controllers\ItemController::class, 'add']);
-    Route::post('/add', [App\Http\Controllers\ItemController::class, 'add']);
-    Route::put('/update/{id}', [App\Http\Controllers\ItemController::class, 'update'])->name('items.update');
-    Route::delete('/items/{id}/destroy', [App\Http\Controllers\ItemController::class, 'destroy'])->name('items.destroy');
-    Route::get('/items/{id}/edit', [App\Http\Controllers\ItemController::class, 'edit'])->name('items.edit');
+Route::middleware('auth')->group(function (){
+    Route::get('/', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+    Route::get('/users', [App\Http\Controllers\UserController::class, 'index'])->name('users.index');
+    Route::get('/items', [App\Http\Controllers\ItemController::class, 'index'])->name('items.index');
 });
-// 編集画面表示
-Route::get('/users/{id}/edit', [App\Http\Controllers\UserController::class, 'edit'])->name('users.edit');
-// 更新処理
-Route::put('/users/{id}', [App\Http\Controllers\UserController::class, 'update'])->name('users.update');
-// 削除ルート（必要に応じて）
-Route::delete('/users/{id}/destroy', [App\Http\Controllers\UserController::class, 'destroy'])->name('users.destroy');
+
+Route::group(['middleware' => ['auth', 'can:admin']], function (){
+
+    Route::get('/news', [App\Http\Controllers\NewsController::class, 'index'])->name('news.index'); // ニュース一覧
+    Route::post('/news', [App\Http\Controllers\NewsController::class, 'store'])->name('news.store'); // ニュース追加
+    Route::delete('/news/{id}', [App\Http\Controllers\NewsController::class, 'destroy'])->name('news.destroy'); // ニュース削除
+
+    Route::prefix('items')->group(function () {
+        Route::get('/add', [App\Http\Controllers\ItemController::class, 'add']);
+        Route::post('/add', [App\Http\Controllers\ItemController::class, 'add']);
+        Route::put('/update/{id}', [App\Http\Controllers\ItemController::class, 'update'])->name('items.update');
+        Route::delete('/items/{id}/destroy', [App\Http\Controllers\ItemController::class, 'destroy'])->name('items.destroy');
+        Route::get('/items/{id}/edit', [App\Http\Controllers\ItemController::class, 'edit'])->name('items.edit');
+    });
+    // 編集画面表示
+    Route::get('/users/{id}/edit', [App\Http\Controllers\UserController::class, 'edit'])->name('users.edit');
+    // 更新処理
+    Route::put('/users/{id}', [App\Http\Controllers\UserController::class, 'update'])->name('users.update');
+    // 削除ルート（必要に応じて）
+    Route::delete('/users/{id}/destroy', [App\Http\Controllers\UserController::class, 'destroy'])->name('users.destroy');
+});    
